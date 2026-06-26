72ec1ca1aa1a315143a9134afaef592c6c5f1206
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-26 17:00:13 +0530

    git commit -m "feat: add array sorted check algorithm"

---
+++ sorted.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bool isSorted = true;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[i - 1]) {
+            isSorted = false;
+            break;
+        }
+    }
+
+    if (isSorted)
+        cout << "Sorted";
+    else
+        cout << "Not Sorted";
+
+    return 0;
+}
